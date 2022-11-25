6036060c1c3d2f39558c6112db54fd2e311be4af
Grcia Jhon Lucas <garciajhonlucas@gmail.com>
2022-11-25 11:40:47 -0500

    Se sube el template de index y persionajes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from app.config import Config
 
+def create_app():
+    aplication = Flask(__name__)
+    aplication.config.from_object(Config)
+    Bootstrap(aplication)
+    return aplication
