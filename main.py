36cdae0855769eb249c77b1624b620fc2b4103df
Grcia Jhon Lucas <garciajhonlucas@gmail.com>
2022-11-25 11:44:36 -0500

    Creacion de rutas y actualizacion de template

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from app import create_app
 
 app = create_app()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/perfil/<int:id>")
+def perfil(id):
+    return render_template("perfil.html")
 
 if __name__ == "__main__":
     app.run()
